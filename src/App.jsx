6da79156d538388bc1b33088c7ff84ac76934c80
VENKAT6912 <vibecoder18@gmail.com>
2025-09-21 03:29:33 +0530

    added sidebar open and close functionality acc to click on home button ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Video from './pages/Video/Video'
 import Home from './pages/Home/Home'
 
 const App = () => {
+
+  const [sidebar,setSidebar]=useState(true);
   return (
     <div>
-      <Navbar/>
+      <Navbar setSidebar={setSidebar}/>
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<Home  sidebar={sidebar}/>}/>
         <Route path='/video/:categoryId/:videoId' element={<Video/>}/>
 
 
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -16,9 +16,9 @@ import megan from '../../assets/megan.png'
 import cameron from '../../assets/cameron.png'
 
 
-const Sidebar = () => {
+const Sidebar = ({sidebar}) => {
   return (
-    <div className='sidebar'>
+    <div className={`sidebar ${sidebar?"":"small-sidebar"}`}>
         <div className="shortcut-links">
             <div className="side-links">
                 <img src={home} alt="" /><p>Home</p>
