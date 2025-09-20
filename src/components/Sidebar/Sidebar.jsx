f57f80f0e8b26540859e278f88deb5f5699ce88e
VENKAT6912 <vibecoder18@gmail.com>
2025-09-21 03:03:04 +0530

    added subscribed channels

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -49,6 +49,26 @@ const Sidebar = () => {
             </div>
             <hr/>
         </div>
+        <div className="subscribed-list">
+            <h3>Subscribed</h3>
+            <div className="side-link">
+                <img src={jack} alt="" /><p>PewDiePie</p>
+            </div>
+            <div className="side-link">
+                <img src={simon} alt="" /><p>MrBeast</p>
+            </div>
+
+            <div className="side-link">
+                <img src={tom} alt="" /><p>Justin Bieber</p>
+            </div>
+            <div className="side-link">
+                <img src={megan} alt="" /><p>5-Minute Crafts</p>
+            </div>
+            <div className="side-link">
+                <img src={cameron} alt="" /><p>Nas Daily</p>
+            </div>
+
+        </div>
 
       
     </div>
