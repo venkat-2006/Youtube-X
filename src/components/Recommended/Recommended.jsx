1f71e9cf3940d87ce74baa17b4430cea6c84434a
VENKAT6912 <vibecoder18@gmail.com>
2025-09-22 03:00:32 +0530

    updating sidebars using api data and ids

--- src/components/Recommended/Recommended.jsx
+++ src/components/Recommended/Recommended.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Recommended.css'
 import thumbnail1 from '../../assets/thumbnail1.png'
 import thumbnail2 from '../../assets/thumbnail2.png'
@@ -8,75 +8,38 @@ import thumbnail5 from '../../assets/thumbnail5.png'
 import thumbnail6 from '../../assets/thumbnail6.png'
 import thumbnail7 from '../../assets/thumbnail7.png'
 import thumbnail8 from '../../assets/thumbnail8.png'
+import { API_KEY, value_converter } from '../../data'
+import { Link } from 'react-router-dom'
 
 
-const Recommended = () => {
+const Recommended = ({ categoryId }) => {
+  const [apiData, setApiData] = useState([]);
+  const fetchData = async () => {
+    const relatedVideo_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=44&regionCode=US&videoCategoryId=${categoryId}&key=${API_KEY}`
+    await fetch(relatedVideo_url).then(res => res.json()).then(data => setApiData(data.items));
+  }
+  useEffect(() => {
+    fetchData();
+
+  }, [])
   return (
     <div className='recommended'>
-      <div className="side-video-list">
-        <img src={thumbnail1} alt="" />
-        <div className='vid-info'>
-            <h4>Channel that gets you into GSoC (Open Source)</h4>
-            <p>Stack-X</p>
-            <p>1.5M Views</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail2} alt="" />
-        <div className='vid-info'>
-            <h4>Channel that gets you into GSoC (Open Source)</h4>
-            <p>Stack-X</p>
-            <p>1.5M Views</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail3} alt="" />
-        <div className='vid-info'>
-            <h4>Channel that gets you into GSoC (Open Source)</h4>
-            <p>Stack-X</p>
-            <p>1.5M Views</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail4} alt="" />
-        <div className='vid-info'>
-            <h4>Channel that gets you into GSoC (Open Source)</h4>
-            <p>Stack-X</p>
-            <p>1.5M Views</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail5} alt="" />
-        <div className='vid-info'>
-            <h4>Channel that gets you into GSoC (Open Source)</h4>
-            <p>Stack-X</p>
-            <p>1.5M Views</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail6} alt="" />
-        <div className='vid-info'>
-            <h4>Channel that gets you into GSoC (Open Source)</h4>
-            <p>Stack-X</p>
-            <p>1.5M Views</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail7} alt="" />
-        <div className='vid-info'>
-            <h4>Channel that gets you into GSoC (Open Source)</h4>
-            <p>Stack-X</p>
-            <p>1.5M Views</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail8} alt="" />
-        <div className='vid-info'>
-            <h4>Channel that gets you into GSoC (Open Source)</h4>
-            <p>Stack-X</p>
-            <p>1.5M Views</p>
-        </div>
-      </div>
+      {apiData.map((item, index) => {
+        return (
+          <Link to={`/video/${item.snippet.categoryId}/${item.id}`}key={index} className="side-video-list">
+            <img src={item.snippet.thumbnails.medium.url} alt="" />
+            <div className='vid-info'>
+              <h4>{item.snippet.title}</h4>
+              <p>{item.snippet.channelTitle}</p>
+              <p>{value_converter(item.statistics.viewCount)} Views</p>
+            </div>
+          </Link>
+
+
+        )
+
+      })}
+
     </div>
   )
 }
