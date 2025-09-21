a35e6ae5e1d7d5dbb685f67639d77235273e08ea
VENKAT6912 <vibecoder18@gmail.com>
2025-09-21 18:21:07 +0530

    added dynamic classname and useState to navigate my sidebar choices

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 import  './Home.css'
 import Sidebar from '../../components/Sidebar/Sidebar'
 import Feed from '../../components/Feed/Feed'
+import { useState } from 'react'
 
 const Home = ({sidebar}) => {
+    const[category,setCategory]=useState(0);
+
+
+
   return (
     <>
-    <Sidebar  sidebar={sidebar}/>
+    <Sidebar  sidebar={sidebar} category={category} setCategory={setCategory}/>
     <div className={`container ${sidebar?"":"large-container"}`}>
-        <Feed/>
+        <Feed category={category} />
 
     </div>
     
