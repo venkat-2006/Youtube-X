18f04004715a03ab46fbd296958fd3c935ffc0c6
VENKAT6912 <vibecoder18@gmail.com>
2025-09-22 17:04:22 +0530

    few bugs fixed in videoplayer and recommended section

--- src/components/Recommended/Recommended.jsx
+++ src/components/Recommended/Recommended.jsx
@@ -9,39 +9,42 @@ import thumbnail6 from '../../assets/thumbnail6.png'
 import thumbnail7 from '../../assets/thumbnail7.png'
 import thumbnail8 from '../../assets/thumbnail8.png'
 import { API_KEY, value_converter } from '../../data'
-import { Link } from 'react-router-dom'
-
+import { Link, useParams } from 'react-router-dom'
 
 const Recommended = ({ categoryId }) => {
   const [apiData, setApiData] = useState([]);
+  const { videoId } = useParams(); //  capture current videoId from route
+
   const fetchData = async () => {
-    const relatedVideo_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=44&regionCode=US&videoCategoryId=${categoryId}&key=${API_KEY}`
-    await fetch(relatedVideo_url).then(res => res.json()).then(data => setApiData(data.items));
-  }
+    const relatedVideo_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=44&regionCode=US&videoCategoryId=${categoryId}&key=${API_KEY}`;
+    await fetch(relatedVideo_url)
+      .then(res => res.json())
+      .then(data => setApiData(data.items || []));
+  };
+
   useEffect(() => {
     fetchData();
+    window.scrollTo(0, 0); //  scroll top whenever video changes
+  }, [categoryId, videoId]); // refetch when category or selected video changes
 
-  }, [])
   return (
-    <div className='recommended'>
-      {apiData.map((item, index) => {
-        return (
-          <Link to={`/video/${item.snippet.categoryId}/${item.id}`}key={index} className="side-video-list">
-            <img src={item.snippet.thumbnails.medium.url} alt="" />
-            <div className='vid-info'>
-              <h4>{item.snippet.title}</h4>
-              <p>{item.snippet.channelTitle}</p>
-              <p>{value_converter(item.statistics.viewCount)} Views</p>
-            </div>
-          </Link>
-
-
-        )
-
-      })}
-
+    <div className="recommended">
+      {apiData.map((item, index) => (
+        <Link
+          to={`/video/${item.snippet.categoryId}/${item.id}`}
+          key={item.id || index}
+          className="side-video-list"
+        >
+          <img src={item.snippet.thumbnails.medium.url} alt={item.snippet.title} />
+          <div className="vid-info">
+            <h4>{item.snippet.title}</h4>
+            <p>{item.snippet.channelTitle}</p>
+            <p>{value_converter(item.statistics.viewCount)} Views</p>
+          </div>
+        </Link>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Recommended
+export default Recommended;
